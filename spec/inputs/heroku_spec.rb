5d1c23566f919b72c649aefd743ac4f67725703c
Vincent Le <vinnyoodles@gmail.com>
2016-08-08 16:16:26 -0400

    add testing

--- spec/inputs/heroku_spec.rb
+++ spec/inputs/heroku_spec.rb
@@ -1,1 +1,28 @@
-require "logstash/devutils/rspec/spec_helper"
+require_relative "../spec_helper"
+
+describe LogStash::Inputs::Heroku do
+
+  before :each do
+    @client = Heroku::Client.new("test", "pw")
+    @input = LogStash::Plugin.lookup("input","heroku").new("app" => "test")
+    allow(Heroku::Client).to receive(:new).and_return(@client)
+  end
+
+  it "should register" do
+    expect{ @input.register }.to_not raise_error
+  end
+
+  context "when operating" do
+
+    before :each do
+      @queue = []
+    end
+
+    it "should read logs" do 
+      allow(@client).to receive(:read_logs).and_yield("LOG LOG LOG LOG")
+      @input.run(@queue)
+      expect(@queue.length).to eq 1
+      expect(@queue.first.to_hash["message"]).to eq "LOG LOG LOG LOG"
+    end
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,4 @@
+# encoding: utf-8
+require "logstash/devutils/rspec/spec_helper"
+require "logstash/inputs/heroku"
+require "heroku/client"
